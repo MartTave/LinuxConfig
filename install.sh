89b4f5e4ad55cc86317ef36d5de7fad24ef0dd1f
MartTave <martin.tavernier256@gmail.com>
2024-09-19 08:55:57 +0200

    wip

--- install.sh
+++ install.sh
@@ -2,6 +2,12 @@
 
 sudo apt install zsh;
 
+mkdir ~/.fonts;
+
+cp ./fonts/*.ttf ~/fonts/;
+
+fc-cache -f -v;
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k;
@@ -20,3 +26,4 @@ brew install btop;
 
 brew install neofetch;
 
+cat ./home/zshrc >> ~/.zshrc;
